c6d80f9795fe4ded42ddb97e6a1258344dc58442
Sunnatillo Shavkatov <sunnatilloshavkatov@gmail.com>
2024-05-10 20:12:50 +0500

    few change

--- lib/core/extension/sound_extension.dart
+++ lib/core/extension/sound_extension.dart
@@ -1,17 +1,17 @@
 part of "extension.dart";
 
-// extension SoundExtension on Sound {
-//   bool get isOn => this == Sound.on;
-// }
-//
-// extension ThemeModeExtensionX on ThemeMode {
-//   bool get isDark => this == ThemeMode.dark;
-//
-//   bool get isLight => this == ThemeMode.light;
-// }
-//
-// extension ThemeModeExtension on bool {
-//   Sound get sound => this ? Sound.on : Sound.off;
-//
-//   ThemeMode get themeMode => this ? ThemeMode.dark : ThemeMode.light;
-// }
+extension SoundExtension on Sound {
+  bool get isOn => this == Sound.on;
+}
+
+extension ThemeModeExtensionX on ThemeMode {
+  bool get isDark => this == ThemeMode.dark;
+
+  bool get isLight => this == ThemeMode.light;
+}
+
+extension ThemeModeExtension on bool {
+  Sound get sound => this ? Sound.on : Sound.off;
+
+  ThemeMode get themeMode => this ? ThemeMode.dark : ThemeMode.light;
+}
