d0d97ceca8ad899524aa4b2591aed684b2ea0dff
Sunnatillo Shavkatov <sunnatilloshavkatov@gmail.com>
2024-05-10 20:01:53 +0500

    few chnage

--- lib/core_module.dart
+++ lib/core_module.dart
@@ -1,10 +1,17 @@
+export "core/app_options.dart";
 export "core/connectivity/network_info.dart";
 export "core/dio_retry/retry_interceptor.dart";
 export "core/either/either.dart";
 export "core/error/failure.dart";
 export "core/error/server_error.dart";
 export "core/extension/extension.dart";
+export "core/l10n/app_localizations_setup.dart";
 export "core/theme/themes.dart";
 export "core/utils/utils.dart";
+export "core/widgets/buttons/custom_loading_button.dart";
+export "core/widgets/custom_cached_network_image.dart";
+export "core/widgets/inputs/custom_password_text_field.dart";
+export "core/widgets/inputs/custom_text_field.dart";
 export "core/widgets/loading/circular_progress_indicator.dart";
 export "core/widgets/loading/modal_progress_hud.dart";
+export "core/widgets/native_splash/flutter_native_splash.dart";
